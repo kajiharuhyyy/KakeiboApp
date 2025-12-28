cf789750d69f04b5b8383860d334effc24fa19e8
kajiharuhyyy <kajiharu0921@gmail.com>
2025-12-29 01:27:43 +0900

    家計簿アプリ作成

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import service.KakeiboService;
 import ui.ConsoleUI;
 
+// 家計簿アプリ
 public class Main {
     public static void main(String[] args) {
         KakeiboService service = new KakeiboService();
